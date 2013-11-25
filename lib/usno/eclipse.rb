e8b990f0d8c2b913b92432bfcd25a960889c1090
Ryan T. Hosford <tad.hosford@gmail.com>
2013-11-25 00:33:46 -0600

    Initial commit. Dependencies include USNO::Eclipse::Solar and USNO::Eclipse::Lunar

---
+++ lib/usno/eclipse.rb
@@ -0,0 +1,7 @@
+require "usno/eclipse/version"
+
+module USNO
+  module Eclipse
+    # Your code goes here...
+  end
+end
---
+++ lib/usno/eclipse/version.rb
@@ -0,0 +1,5 @@
+module USNO
+  module Eclipse
+    VERSION = "0.0.1"
+  end
+end
